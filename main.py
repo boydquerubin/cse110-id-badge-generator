5bd692b93251ac25c94a6456fc0c9fffc97f5bf0
Boyd Querubin <boydisaacq@gmail.com>
2024-09-17 17:28:24 -0600

    formatted phone number

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 first_name = input("Enter your first name: ").title()
 last_name = input("Enter your last name: ").upper()
 email = input("Enter your email address: ")
-phone_number = int(input("Enter your phone number: "))
+
+phone_number = input("Enter your phone number: ")
+formatted_phone_number = f"({phone_number[:3]}) {phone_number[3:6]}-{phone_number[6:]}"
+
 job_title = input("Enter your job title: ").title()
 id_number = int(input("Enter your ID Number: "))
 hair_color = input("Enter your hair color: ").title()
@@ -16,7 +19,7 @@ print(f"{last_name}, {first_name}")
 print(f"{job_title}")
 print(f"ID: {id_number}\n")
 print(f"{email}")
-print(f"{phone_number[0, 3]}\n")
+print(f"{phone_number}\n")
 print(f"Hair: {hair_color:<15} Eyes: {eye_color}")
 print(f"Month: {month:<15} Training: {training}")
 print("-" * 40)
